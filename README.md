f1d3f98dfdc2dfa1e3a3c2fd0121c5f29f771fd7
Stephen Muema <105632674+Stephen11221@users.noreply.github.com>
2025-01-02 11:00:38 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Audio-pine-solution
---
+++ assets/script.js
@@ -0,0 +1,5 @@
+
+// Stop the redirection
+    // JavaScript to display the year
+    const currentYear = new Date().getFullYear();
+    document.getElementById('year').textContent = currentYear;
